e2ecd5be31fb1e8fccb72f06ff699452d2615ed3
Miguel Fajardo <majfe202@hotmail.com>
2025-12-31 14:44:29 -0700

    router

--- lib/core/router/app_router.dart
+++ lib/core/router/app_router.dart
@@ -1,3 +1,6 @@
+import 'package:crv_reprosisa/features/dashboard/presentation/pages/admin/admin_dashboard_page.dart';
+import 'package:crv_reprosisa/features/dashboard/presentation/pages/dashboard_entry_page.dart';
+import 'package:crv_reprosisa/features/dashboard/presentation/pages/technician/technician_dashboard_page.dart';
 import 'package:go_router/go_router.dart';
 import '../../features/auth/presentation/pages/login_page.dart';
 
@@ -5,7 +8,19 @@ final appRouter = GoRouter(
   routes: [
     GoRoute(
       path: '/',
-      builder: (context, state) =>  const LoginPage()
+      builder: (context, state) {
+        return const DashboardEntryPage(role: UserRole.admin);
+      },
+      routes: [
+        GoRoute(
+          path: 'admin/dashboard',
+          builder: (context, state) => const AdminDashboardPage(),
+        ),
+        GoRoute(
+          path: 'technician/dashboard',
+          builder: (context, state) => const TechnicianDashboardPage()
+        )
+      ]
     )
   ]
 );
