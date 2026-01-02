aae7395bb463d024ff4a137ce6230dbd4775c7c5
Miguel Fajardo <majfe202@hotmail.com>
2026-01-02 13:02:07 -0700

    routes created

---
+++ lib/core/router/app_routes.dart
@@ -0,0 +1,8 @@
+abstract class AppRoutes {
+  static const login = "/login";
+  static const dashboard = "/dashboard";
+  static const inspections = "/inspections";
+  static const reports = "/reports";
+  static const users = "/users";
+  static const assets = "/assets";
+}
