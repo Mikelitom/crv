fc53aa7e2ba4c4cd1d4e9d93b15a81f5ee61bce4
Miguel Fajardo <majfe202@hotmail.com>
2026-01-05 14:31:35 -0700

    router

--- lib/core/router/app_router.dart
+++ lib/core/router/app_router.dart
@@ -1,26 +1,103 @@
-import 'package:crv_reprosisa/features/dashboard/presentation/pages/admin/admin_dashboard_page.dart';
-import 'package:crv_reprosisa/features/dashboard/presentation/pages/dashboard_entry_page.dart';
-import 'package:crv_reprosisa/features/dashboard/presentation/pages/technician/technician_dashboard_page.dart';
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
+
+
+// Dev
+import 'app_routes.dart';
+import '../../features/dev/presentation/pages/dev_role_selector_page.dart';
+
+// Auth
 import '../../features/auth/presentation/pages/login_page.dart';
 
-final appRouter = GoRouter(
-  routes: [
-    GoRoute(
-      path: '/',
-      builder: (context, state) {
-        return const DashboardEntryPage(role: UserRole.admin);
-      },
-      routes: [
+// Core Pages
+import '../../features/dashboard/presentation/pages/dashboard_entry_page.dart';
+import '../../features/inspections/pages/inspections_page.dart';
+// import '../../features/reports/pages/reports_page.dart';
+
+// Admin only pages
+// import '../../features/users/pages/users_page.dart';
+// import '../../features/assets/pages/assets_page.dart';
+
+class AppRouter {
+  static final GoRouter router = GoRouter(
+    debugLogDiagnostics: kDebugMode,
+
+    initialLocation: kDebugMode
+      ? AppRoutes.devUser
+      : AppRoutes.login,
+
+    redirect: (context, state) {
+      // TODO: Implement Auth
+      // final loggedIn = Auth.isLoggedIn;
+      final location = state.matchedLocation;
+
+      final isLogin = location == AppRoutes.login;
+      final isDevRoute = location == AppRoutes.devUser;
+      final isDebug = kDebugMode;
+
+      // if (!loggedId) {
+      //   if (isDebug && isDevRoute) return null;
+      //   if (!isLogin) return AppRoutes.login;
+      // }
+
+      // if (loggedIn && (isLogin || isDevRoute)) {
+      //   return AppRoutes.dashboard;
+      // }
+
+      return null;
+    },
+
+    routes: [
+      GoRoute(
+        path: AppRoutes.login,
+        builder: (context, state) => const LoginPage(),
+      ),
+      // DEV user selector (solo debug)
+      if (kDebugMode)
         GoRoute(
-          path: 'admin/dashboard',
-          builder: (context, state) => const AdminDashboardPage(),
+          path: AppRoutes.devUser,
+          builder: (context, state) => const DevUserSelectorPage(),
         ),
-        GoRoute(
-          path: 'technician/dashboard',
-          builder: (context, state) => const TechnicianDashboardPage()
-        )
-      ]
-    )
-  ]
-);
+
+      // Dashboard
+      GoRoute(
+        path: AppRoutes.dashboard,
+        builder: (context, state) => const DashboardPage(),
+      ),
+
+      // Inspections
+      GoRoute(
+        path: AppRoutes.inspections,
+        builder: (context, state) => const InspectionsPage(),
+      ),
+
+      // Reports
+      GoRoute(
+        path: AppRoutes.reports,
+        builder: (context, state) => const ReportsPage(),
+      ),
+
+      // Users (admin UI decides access)
+      GoRoute(
+        path: AppRoutes.users,
+        builder: (context, state) => const UsersPage(),
+      ),
+
+      // Assets (admin UI decides access)
+      GoRoute(
+        path: AppRoutes.assets,
+        builder: (context, state) => const AssetsPage(),
+      ),
+    ],
+
+    // Ruta no encontrada
+    errorBuilder: (context, state) => const Scaffold(
+      body: Center(
+        child: Text('Ruta no encontrada'),
+      ),
+    ),
+  );
+
+
+}
