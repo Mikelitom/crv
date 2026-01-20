b0531746411dea63df0cfb99041118e7f9a6d89b
Miguel Fajardo <majfe202@hotmail.com>
2026-01-20 11:42:55 -0700

    reports tiene la tabla general

---
+++ lib/features/reports/models/report_row_ui.dart
@@ -0,0 +1,11 @@
+class ReportRowUI {
+  final String tipo;
+  final String titulo;
+  final String fecha;
+
+  const ReportRowUI({
+    required this.tipo,
+    required this.titulo,
+    required this.fecha,
+  });
+}
