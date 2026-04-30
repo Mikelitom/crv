079f7406bed4095fd4b0dac530680ef1cbce65c2
Miguel Fajardo <majfe202@hotmail.com>
2026-04-30 14:33:39 -0700

    fix

--- lib/core/config/environment.dart
+++ lib/core/config/environment.dart
@@ -15,7 +15,7 @@ class EnvironmentConfig {
         );
       default:
         return EnvironmentConfig._(
-          'https://backend-crv-refactor.onrender.com/api/v1',
+          'http://127.0.0.1:8000/api/v1',
         );
     }
   }
