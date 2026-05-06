a7431b6ce853f3b8eb0e36174a93a154701aadd3
Miguel Fajardo <majfe202@hotmail.com>
2026-05-06 15:07:14 -0700

    fix address

--- lib/features/assets/domain/params/create_clients_params.dart
+++ lib/features/assets/domain/params/create_clients_params.dart
@@ -3,13 +3,11 @@ class CreateClientParams {
   final String company;
   final String? phone;
   final String email;
-  final String address;
 
   const CreateClientParams({
     required this.name,
     required this.company,
     this.phone,
     required this.email,
-    required this.address,
   });
 }
