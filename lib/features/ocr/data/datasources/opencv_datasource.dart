cdaf1f0198b82c56681a84c3d6b4caf7752c21bc
Miguel Fajardo <majfe202@hotmail.com>
2026-07-27 13:19:26 -0700

    changes

--- lib/features/ocr/data/datasources/opencv_datasource.dart
+++ lib/features/ocr/data/datasources/opencv_datasource.dart
@@ -1,8 +1,9 @@
+import 'package:crv_reprosisa/features/ocr/domain/entities/processed_image.dart';
 import 'package:opencv_dart/opencv_dart.dart' as cv;
 
 class OpenCVDataSource {
 
-  Future<String> processImage(String imagePath) async {
+  Future<ProcessedImage> processImage(String imagePath) async {
 
     // Leer imagen
     final image = cv.imread(imagePath);
@@ -16,13 +17,13 @@ class OpenCVDataSource {
     // Reducir ruido
     final blur = cv.gaussianBlur(
       gray,
-      (5,5),
+      (5, 5),
       0,
     );
 
-    // Blanco y negro
+    // Adaptive Threshold
     final threshold = cv.adaptiveThreshold(
-      gray,
+      blur,
       255,
       cv.ADAPTIVE_THRESH_GAUSSIAN_C,
       cv.THRESH_BINARY,
@@ -30,16 +31,41 @@ class OpenCVDataSource {
       10,
     );
 
-    final outputPath = imagePath.replaceFirst(
-      ".jpg",
-      "_processed.jpg",
-    );
+    // Rutas
+    final grayPath = _generatePath(imagePath, "gray");
+    final blurPath = _generatePath(imagePath, "blur");
+    final thresholdPath = _generatePath(imagePath, "threshold");
 
-    cv.imwrite(
-      outputPath,
-      threshold,
-    );
+    // Guardar imágenes
+    cv.imwrite(grayPath, gray);
+    cv.imwrite(blurPath, blur);
+    cv.imwrite(thresholdPath, threshold);
 
-    return outputPath;
+    // Regresar entidad
+    return ProcessedImage(
+      originalPath: imagePath,
+      grayPath: grayPath,
+      blurPath: blurPath,
+      thresholdPath: thresholdPath,
+      cannyPath: null,
+      contourPath: null,
+      perspectivePath: null,
+    );
+  }
+
+  String _generatePath(
+    String originalPath,
+    String suffix,
+  ) {
+    final index = originalPath.lastIndexOf(".");
+
+    if (index == -1) {
+      return "${originalPath}_$suffix";
+    }
+
+    final name = originalPath.substring(0, index);
+    final extension = originalPath.substring(index);
+
+    return "${name}_$suffix$extension";
   }
 }
